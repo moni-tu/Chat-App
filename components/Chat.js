b3f4e34ef2ca637897d904ac99cf42063e329c28
moni-tu <turani.monica@gmail.com>
2022-04-19 15:54:54 +0200

    re-factor: use react-native-gifted-chat to make chat.js functional

--- components/Chat.js
+++ components/Chat.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
 
 
 export default class Chat extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
+
   render() {
-    //entered name state from Start screen gets displayed in status bar at the top of the app
-    let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name});
-
-    const { bgColor } = this.props.route.params;
-
     return (
-      <View style={{
-        flex: 1, 
-        justifyContent: 'center', 
-        alignItems: 'center',
-        backgroundColor: bgColor
-        }}>
-        <Text>Hello Chat!</Text>
-      </View>
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
     )
   }
 }
