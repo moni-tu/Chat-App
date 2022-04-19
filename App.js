dabec787a390dde02468bc9c42be6812993404b9
moni-tu <91124224+moni-tu@users.noreply.github.com>
2022-04-19 13:12:35 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TextInput, Button, Alert, ScrollView } from 'react-native';
+// importing screens we want to navigate between
+import Start from './components/Start';
+import Chat from './components/Chat';
+
+// import react native gesture handler
+import 'react-native-gesture-handler';
+
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// create navigator
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+ render() {
+   return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Start"
+      >
+        <Stack.Screen
+          name="Start" component={Start}
+        />
+        <Stack.Screen
+          name="Chat" component={Chat}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+   );
+ }
+}
+
+// style sheet at the bottom
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
