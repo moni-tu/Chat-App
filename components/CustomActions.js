38224b81dd4037d507e4fe357dee6bf0a786a2a4
moni-tu <turani.monica@gmail.com>
2022-04-30 17:37:32 +0200

    feat: add TouchableOpacity in render function and styles

---
+++ components/CustomActions.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
+
+export default class CustomActions extends React.Component {
+
+    render() {
+        return (
+          <TouchableOpacity style={[styles.container]} onPress={this.onActionPress}>
+            <View style={[styles.wrapper, this.props.wrapperStyle]}>
+              <Text style={[styles.iconText, this.props.iconTextStyle]}>+</Text>
+            </View>
+          </TouchableOpacity>
+        );
+      }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      width: 26,
+      height: 26,
+      marginLeft: 10,
+      marginBottom: 10,
+    },
+    wrapper: {
+      borderRadius: 13,
+      borderColor: '#b2b2b2',
+      borderWidth: 2,
+      flex: 1,
+    },
+    iconText: {
+      color: '#b2b2b2',
+      fontWeight: 'bold',
+      fontSize: 16,
+      backgroundColor: 'transparent',
+      textAlign: 'center',
+    },
+   });
