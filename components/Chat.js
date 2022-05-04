6c52a4534e34013c6af77317484b216d802e9ca1
moni-tu <turani.monica@gmail.com>
2022-05-04 13:21:35 +0200

    feat: add  renderCustomView(props)

--- components/Chat.js
+++ components/Chat.js
@@ -204,11 +204,30 @@ export default class Chat extends React.Component {
       return <InputToolbar {...props} />;
     }
   }
-
+  // displays the communication features
   renderCustomActions = (props) => {
     return <CustomActions {...props} />;
   };
 
+  //custom map view
+  renderCustomView(props) {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      );
+    }
+    return null;
+  }
+  //render components
   render() {
     return (
       <View style={styles.chatView}>
